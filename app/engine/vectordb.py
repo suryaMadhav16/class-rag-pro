74d4357ac4fedcfb658c6107274ff5ba79ea2854
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2025-03-14 11:36:10 -0400

    Initial commit from Create Llama

---
+++ app/engine/vectordb.py
@@ -0,0 +1,25 @@
+import os
+
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+
+def get_vector_store():
+    collection_name = os.getenv("CHROMA_COLLECTION", "default")
+    chroma_path = os.getenv("CHROMA_PATH")
+    # if CHROMA_PATH is set, use a local ChromaVectorStore from the path
+    # otherwise, use a remote ChromaVectorStore (ChromaDB Cloud is not supported yet)
+    if chroma_path:
+        store = ChromaVectorStore.from_params(
+            persist_dir=chroma_path, collection_name=collection_name
+        )
+    else:
+        if not os.getenv("CHROMA_HOST") or not os.getenv("CHROMA_PORT"):
+            raise ValueError(
+                "Please provide either CHROMA_PATH or CHROMA_HOST and CHROMA_PORT"
+            )
+        store = ChromaVectorStore.from_params(
+            host=os.getenv("CHROMA_HOST"),
+            port=os.getenv("CHROMA_PORT", "8001"),
+            collection_name=collection_name,
+        )
+    return store
